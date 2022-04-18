dd5a807220633fafa4ad7bc0ed5139bbdbcd1e64
AntoninaVish <vishivtonya@gmail.com>
2022-04-18 21:18:21 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITrainingSelection/WallFilter.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingSelection
+{
+    public class WallFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            return elem is Wall; // выбираем только стены в фильтре
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+}
